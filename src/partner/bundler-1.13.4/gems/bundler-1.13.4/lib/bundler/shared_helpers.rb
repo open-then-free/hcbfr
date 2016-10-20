ca797c45a9d39ebfcec7d27aa8f07fcf2a9221fc
Dewa Rama <dewa.rama@merqurion.com>
2016-10-20 17:04:19 +0700

    Apply some changes on source files.

--- src/partner/bundler-1.13.4/gems/bundler-1.13.4/lib/bundler/shared_helpers.rb
+++ src/partner/bundler-1.13.4/gems/bundler-1.13.4/lib/bundler/shared_helpers.rb
@@ -37,10 +37,10 @@ module Bundler
     end
 
     def default_bundle_dir
-      bundle_dir = find_directory(".bundle")
+      bundle_dir = find_directory(".gap/depmgr")
       return nil unless bundle_dir
 
-      global_bundle_dir = File.join(Bundler.rubygems.user_home, ".bundle")
+      global_bundle_dir = File.join(Bundler.rubygems.user_home, ".gap/depmgr")
       return nil if bundle_dir == global_bundle_dir
 
       Pathname.new(bundle_dir)
